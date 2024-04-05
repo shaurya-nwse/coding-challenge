d0c9f18de0dc47105b9db2487e827d264baea588
shaurya-nwse <shauryarawat241994@gmail.com>
2024-04-05 12:49:03 +0200

    Add argument parser for CLI

--- performance/performance/benchmark.md
+++ performance/performance/benchmark.md
@@ -6,28 +6,52 @@
 - Polars: 7-10 seconds
 - DuckDB (write to JSON): 3-4 seconds
 
-**- DuckDB (write to parquet per thread): 3-3.2 seconds - FASTEST**
+> **DuckDB (write to parquet per thread): 3-3.2 seconds - FASTEST**
 
 - Dask Dataframe (unoptimized): 1457 seconds
 
-
 # Instructions to run
+
 1. Install poetry in your python environment
+
 ```shell
 pip install poetry
 ```
 
 2. Install the dependencies:
+
 ```shell
 poetry install
 ```
 
 3. Generate the dataset
+
 ```shell
 python data_generator.py
 ```
+
 Make sure its in the root of the project i.e. where the `requirements.txt` is.
 
 4. Run the program:
+   `engine` can be one of:
 
+- polars
+- duckdb
+- pandas
+- pandas_naive
+- naive
+- dask
 
+if you choose `duckdb`, make sure to supply the format (either of `parquet` or `json`)
+
+```shell
+# for json output using the duckdb engine
+python performance/main.py --engine duckdb --format json
+
+# using polars
+python performance/main.py --engine polars
+```
+
+5. Inspect the output as `result_<engine>.json` where `<engine>` would be the engine you chose
+
+In case of any questions, reach out to `rawatshaurya1994@gmail.com`.
--- performance/performance/main.py
+++ performance/performance/main.py
@@ -4,6 +4,8 @@ import json
 from pathlib import Path
 import duckdb
 
+from argparse import ArgumentParser
+
 from performance.process_file import (
     process_naive,
     process_pandas,
@@ -17,23 +19,66 @@ from performance.process_file import (
 if __name__ == "__main__":
     root = Path(__file__).parent.parent
     data_path = str(root / "transactions.json")
+
+    parser = ArgumentParser()
+
+    parser.add_argument("-e", "--engine")
+    parser.add_argument("-f", "--format")
+
+    args = parser.parse_args()
+
+    engine_fn_map = {
+        "naive": process_naive,
+        "pandas": process_pandas,
+        "pandas_naive": process_pandas_naive,
+        "polars": process_polars,
+        "duckdb": process_quack,
+        "dask": process_dask,
+    }
+
+    if not args.engine:
+        raise Exception("Please specify the engine type with --engine")
+
+    engine = args.engine.strip().lower()
+
+    if engine not in engine_fn_map:
+        raise Exception(
+            f"Unknown engine, supported engines are: {', '.join(engine_fn_map.keys())}"
+        )
+
+    fn = engine_fn_map[engine]
+
     start = perf_counter()
-    # results = process_pandas(data_path)
-    # results = process_polars(data_path)
-    results = process_dask(data_path)
+    results = fn(data_path)
 
-    # duckdb.sql(f"""copy results to '{str(root / "result_duckdb.json")}'""")
-    # duckdb.sql(
-    #     f"""copy results to '{str(root / "result_duckdb_alt")}'
-    #     (format parquet, per_thread_output true, overwrite_or_ignore true)"""
-    # )
+    if engine == "naive":
+        with open(root / "result_naive.json", "w") as f:
+            f.write(json.dumps(results))
+    elif engine == "pandas" or engine == "pandas_naive":
+        results.to_json(str(root / "result_pandas.json"), index=False, orient="records")
+    elif engine == "polars":
+        results.write_json(str(root / "result_polars.json"), row_oriented=True)
+    elif engine == "duckdb":
+        file_format = args.format
+        if not file_format or file_format.strip().lower() not in ("parquet", "json"):
+            print(
+                "File format not provided or not supported. Defaulting to  parquet; only parquet and json are supported"
+            )
+            file_format = "parquet"
+        else:
+            file_format = args.format.strip().lower()
+
+        if file_format == "parquet":
+            duckdb.sql(
+                f"""copy results to '{str(root / "result_duckdb")}' (format parquet, per_thread_output true, overwrite_or_ignore true)"""
+            )
+        else:
+            duckdb.sql(
+                f"""copy results to '{str(root / "result_duckdb.json")}' (format  json)"""
+            )
+    else:
+        results.to_json(str(root / "result_dask.json"), index=False, orient="records")
+
     end = perf_counter()
+
     print(f"Time: {end - start}")
-    # results.write_json(str(root / "result_polars.json"), row_oriented=True)
-    # results.write_json(str(root / "result_polars_mp.json"), row_oriented=True)
-    results.to_json(str(root / "result_dask.json"), index=False, orient="records")
-
-    # results.to_json(str(root / "result_pandas_v2.json"), index=False, orient="records")
-
-    # with open(root / "result_naive.json", "w") as f:
-    #     f.write(json.dumps(results))
